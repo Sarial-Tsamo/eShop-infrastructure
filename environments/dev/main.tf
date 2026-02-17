0136ea29e69fd2d33bf86e9084eb00df814b2a1d
SarialBebeto <sarial.tsamo@outlook.de>
2026-02-17 19:49:17 +0100

    vpc added to dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,3 +5,9 @@ provider "aws" {
 locals {
     name_prefix = "eshop-${var.environment}"
 }
+
+module "vpc" {
+    source = "../../modules/vpc"
+
+    name_prefix = local.name_prefix
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -2,3 +2,15 @@ variable "name_prefix" {
     description = "Prefix for all resource names"
     type        = string
 }
+
+variable "cidr_block" {
+    description = "CIDR block for the VPC"
+    type        = string
+    default     = "10.0.0.0/16"
+}
+
+variable "az_count" {
+    description = "Number of Availability Zones to use"
+    type        = number
+    default     = 2
+}
