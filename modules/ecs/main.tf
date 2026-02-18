bdbf7e7a4cb10445dbed2058298afc2ca733260f
SarialBebeto <sarial.tsamo@outlook.de>
2026-02-18 16:16:18 +0100

    error fixed

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,4 +1,4 @@
-resource "aws_ecs_cluster" "this" {
+resource "aws_ecs_cluster" "cluster" {
     name = "${var.name_prefix}-cluster"
 }
 
@@ -81,7 +81,7 @@ resource "aws_ecs_task_definition" "this" {
 
 resource "aws_ecs_service" "this" {
     name                 = "${var.name_prefix}-service"
-    cluster              = aws_ecs_cluster.this.id
+    cluster              = aws_ecs_cluster.cluster.id
     task_definition      = aws_ecs_task_definition.this.arn
     launch_type           = "FARGATE"
     desired_count        = 1
