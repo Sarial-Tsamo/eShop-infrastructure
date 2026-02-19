f040269bc5bc18ad2d660625fc78095bd566c5bf
SarialBebeto <sarial.tsamo@outlook.de>
2026-02-19 18:30:20 +0100

    error on vpc module fixed

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,22 +1,97 @@
-module "vpc" {
-    source = "../../modules/vpc"
-    version = "~> 5.0"
+data "aws_availability_zones" "available" {}
 
-    name_prefix = var.name_prefix
-    cidr_block = "10.0.0.0/16"
+locals {
+    azs = slice(data.aws_availability_zones.available.names, 0, 2)
+}
 
-    azs = [
-        "${var.aws_region}a",
-        "${var.aws_region}b"
-    ]
+########################
+# VPC
+########################
 
-    public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-    private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
-
-
-    enable_nat_gateway = false
-    single_nat_gateway = false
-
+resource "aws_vpc" "this" {
+    cidr_block        = var.vpc_cidr_block
     enable_dns_hostnames = true
     enable_dns_support   = true
+
+    tags = {
+        Name = "${var.name_prefix}-vpc"
+    }
 }
+
+########################
+# Public Subnets
+########################
+
+resource "aws_subnet" "public" {
+   count  = 2
+
+   vpc_id                   = aws_vpc.this.id
+   cidr_block               = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+   availability_zone        = local.azs[count.index]
+   map_public_ip_on_launch  = true
+
+   tags = {
+     Name = "${var.name_prefix}-public-${count.index + 1}"
+    }
+}
+
+#########################
+# Private Subnets
+#########################
+
+resource "aws_subnet" "private" {
+
+  count = 2
+
+  vpc_id             = aws_vpc.this.id
+  cidr_block         = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
+  availability_zone  = local.azs[count.index]
+
+  tags = {
+    Name = "${var.name_prefix}-private-${count.index + 1}"
+  }
+} 
+
+##########################
+# Public Route Table
+##########################
+
+resource "aws_route_table" "public" {
+ vpc_id   = aws_vpc.this.id
+
+ tags = { 
+  Name = "${var.name_prefix}-public-rt"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id           = aws_route_table.public.id
+  destination_cidr_block   = "0.0.0.0/0"
+  gateway_id               = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count = 2
+  subnet_id         = aws_subnet.public[count.index].id
+  route_table_id    = aws_route_table.public.id
+}
+
+##########################
+# Private Route Table
+##########################
+
+resource "aws_route_table" "private" {
+  vpc_id  = aws_vpc.this.id
+
+  tags =  {
+   Name  = "${var.name_prefix}-private-rt"
+   }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count = 2
+
+  subnet_id   = aws_subnet.private[count.index].id
+  route_table_id  = aws_route_table.private.id
+}
+
